581874ff5e3b4612dcbb715d55b8e80a7f90b5ed
bakonyig <bakonyi.gergo.istvan@gmail.com>
2019-10-03 01:26:29 +0200

    ASD

--- kafka/creaste-kafka_topic.sh
+++ kafka/creaste-kafka_topic.sh
@@ -2,3 +2,9 @@
 
 cd /usr/hdp/current/kafka-broker/bin
 ./kafka-topics.sh --create --zookeeper sandbox-hdp.hortonworks.com:2181 --replication-factor 1 --partitions 1 --topic bkk
+
+# Kafka topicok listázása
+./kafka-topics.sh --list --zookeeper sandbox-hdp.hortonworks.com:2181
+
+# Console Consumer indítása
+./kafka-console-consumer.sh --zookeeper sandbox-hdp.hortonworks.com:2181 --bootstrap-server localhost:9092 --topic bkk --from-beginning
