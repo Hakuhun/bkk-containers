8e53fee3af16d28da2cc326c74840cb96418a6b1
bakonyig <bakonyi.gergo.istvan@gmail.com>
2019-10-03 00:34:39 +0200

    Kafka és hasznos commandok

--- bkk-initial-route-database/init.bat
+++ bkk-initial-route-database/init.bat
@@ -1,5 +1,3 @@
-cd C:\DEV\hadoop\bkk-containers\bkk-initial-route-database
-
 docker build -t bkk/mysql .
 
 docker-compose -p bkk up
---
+++ kafka/creaste-kafka_topic.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+cd /usr/hdp/current/kafka-broker/bin
+./kafka-topics.sh --create --zookeeper sandbox-hdp.hortonworks.com:2181 --replication-factor 1 --partitions 1 --topic bkk
